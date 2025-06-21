dfda07f7c97ed7d9cdbd86a1eac91a10067cf074
akleem-AA <akleem789@gmail.com>
2025-06-21 11:17:05 +0530

    fixed some error ni android for runing

--- lib/view/screens/checkout/payment_webview_screen.dart
+++ lib/view/screens/checkout/payment_webview_screen.dart
@@ -86,7 +86,7 @@ class PaymentScreenState extends State<PaymentWebViewScreen> {
         body: Stack(
           children: [
             InAppWebView(
-              initialUrlRequest: URLRequest(url: Uri.parse(selectedUrl)),
+              initialUrlRequest: URLRequest(url: WebUri(Uri.parse(selectedUrl).toString())),
               initialUserScripts: UnmodifiableListView<UserScript>([]),
               pullToRefreshController: pullToRefreshController,
               initialOptions: InAppWebViewGroupOptions(crossPlatform: InAppWebViewOptions(
